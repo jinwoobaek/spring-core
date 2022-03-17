dc3cd1242af21cb5a4bcda2208aba3f89ccf1037
Jinwoo Baek <back3946@gmail.com>
2022-03-17 18:28:40 +0900

    AppConfig refactoring

--- src/main/java/hello/springcore/AppConfig.java
+++ src/main/java/hello/springcore/AppConfig.java
@@ -1,19 +1,32 @@
 package hello.springcore;
 
+import hello.springcore.discount.DiscountPolicy;
 import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.member.MemberRepository;
 import hello.springcore.member.MemberService;
 import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.member.MemoryMemberRepository;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 구현체 선택
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 구현체 선택
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
